65757aefe2755dd141df786f8e9aacaddc54e7fe
Vijay Patel <spiritedtechie@gmail.com>
2024-03-01 17:19:04 +0000

    Adds in basic websockets server

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,15 +1,34 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 import dotenv from "dotenv";
+import WebSocket from 'ws';
 
 dotenv.config();
 
-const app:Express = express(); 
-const port = process.env.PORT;
+const api:Express = express(); 
+const api_port = process.env.API_PORT;
 
-app.get('/', (req, res) => {
+const wss_port: number = parseInt(process.env.WSS_PORT || "3001");
+const wss = new WebSocket.Server({ port: wss_port });
+
+wss.on('connection', (ws: WebSocket) => {
+  console.log('New client connected');
+
+  ws.on('message', (message: string) => {
+    console.log(`Received message: ${message}`);
+    ws.send(`Server received your message: ${message}`);
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
+
+api.get('/', (req, res) => {
   res.send('Express + TypeScript Server');
 });
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+api.listen(api_port, () => {
+  console.log(`[server]: Server is running at http://localhost:${api_port}`);
 });
+
+
