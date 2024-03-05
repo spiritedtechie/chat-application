7245b5e67c6d9b7a28ef44b4f0f368bb1df7a8d6
Vijay Patel <spiritedtechie@gmail.com>
2024-03-05 11:59:42 +0000

    Joins up websockets API to test kafka topic

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
 import WebSocket from 'ws';
+import { Kafka, KafkaConfig, EachMessagePayload } from 'kafkajs';
 
 dotenv.config();
 
@@ -10,18 +11,8 @@ const api_port = process.env.API_PORT;
 const wss_port: number = parseInt(process.env.WSS_PORT || "3001");
 const wss = new WebSocket.Server({ port: wss_port });
 
-wss.on('connection', (ws: WebSocket) => {
-  console.log('New client connected');
-
-  ws.on('message', (message: string) => {
-    console.log(`Received message: ${message}`);
-    ws.send(`Server received your message: ${message}`);
-  });
-
-  ws.on('close', () => {
-    console.log('Client disconnected');
-  });
-});
+const kafkaConfig: KafkaConfig = { brokers: ['localhost:9093'] }
+const kafka = new Kafka(kafkaConfig)
 
 api.get('/', (req, res) => {
   res.send('Express + TypeScript Server');
@@ -32,3 +23,41 @@ api.listen(api_port, () => {
 });
 
 
+wss.on('connection', async (ws: WebSocket) => {
+  console.log('New client connected');
+
+  const producer = kafka.producer()
+  const consumer = kafka.consumer({ groupId: 'test-group' })
+
+  await consumer.connect()
+  await consumer.subscribe({ topic: 'test-topic', fromBeginning: true })
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }: EachMessagePayload) => {
+      console.log(message.value?.toString())
+      if (message.value) {
+        ws.send(message.value?.toString())
+      }
+    },
+  })
+
+  ws.on('message', async (message: string) => {
+    console.log(`Received message: ${message}`);
+    await producer.connect()
+    await producer.send({
+      topic: 'test-topic',
+      messages: [
+        { value: message as string },
+      ],
+    })
+
+    ws.send(`Server received your message: ${message}`);
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');    
+    producer.disconnect()
+    consumer.disconnect()
+  });
+
+  // detect broken connections and close
+});
